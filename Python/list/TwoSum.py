5f5f1b0ad642f59d6cb3f15de8ffc6af0282584d
sivaprakash622003@gmail.com <GitSiva@9432.>
2025-09-15 09:05:17 -0700

    initial update

--- Python/list/TwoSum.py
+++ Python/list/TwoSum.py
@@ -10,9 +10,9 @@ print(twoSum(nums,7))
 Solved !------
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)-1):
-                if(nums[i]+nums[j+1]==target):
-                    return i,j+1
+      for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if(nums[i]+nums[j]==target):
+                return [i,j] 
 
 """
