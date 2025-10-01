c7004c5cebfb0e9d95b5f60c917a3c29c5fbcd9f
sivaprakash622003@gmail.com <GitSiva@9432.>
2025-10-01 07:46:28 -0700

    feat:create new folder

---
+++ Python/HackerRank/if_else.py
@@ -0,0 +1,51 @@
+"""Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+Explanation 0
+
+
+ is odd and odd numbers are weird, so print Weird.
+
+Sample Input 1
+
+24
+Sample Output 1
+
+Not Weird """
+
+#solution
+
+
+import main_file
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    if n%2==1:
+        print("Weird")
+    else:
+        if(2<=n<=5):
+            print("Not Weird")
+        elif(6<=n<=20):
+            print("Weird")
+        elif(n>20):
+            print("Not Weird")
+
---
+++ Python/HackerRank/main_file.py
@@ -0,0 +1,3 @@
+print("hii")
+if __name__ =='__main__':
+    print("sivaprakash")
